393a3ddf9b147449038a16538321b4eb0db53b48
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-03-29 18:02:02 +0900

    タイトルシーン完成

--- Assets/Script/RotateCam.cs
+++ Assets/Script/RotateCam.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class RotateCam : MonoBehaviour
 {
-    [SerializeField] private float speed = 10.0f; // 回転速度
-    [SerializeField] private Camera cam;        // カメラ
+    [SerializeField] private float speed = 10.0f;   // 回転速度
+    [SerializeField] private Camera cam;            // カメラ
+    static private Quaternion camRot;               // カメラの回転
+
+    void Start()
+    {
+        cam.transform.rotation = camRot;            // カメラの回転を設定
+    }
+
     void Update()
     {
         // カメラを回転させる
-        cam.transform.Rotate(Vector3.up, speed * Time.deltaTime);    
+        cam.transform.Rotate(Vector3.up, speed * Time.deltaTime); 
+        camRot = cam.transform.rotation;            // カメラの回転を取得   
     }
 }
--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -8,6 +8,7 @@ public class Title : MonoBehaviour
     [SerializeField] private float lodadDelay = 1.0f; // ロード遅延時間
     [SerializeField] private string sceneName = "Game"; // ロードするシーン名
     [SerializeField] private AudioClip start; // 音声クリップ
+    [SerializeField] private GameObject logo; // タイトルオブジェクト
     private AudioSource audioSource; // AudioSourceコンポーネント
     private bool isLoading = false; // シーンがロード中かどうか
 
@@ -19,13 +20,15 @@ public class Title : MonoBehaviour
 
     void Update()
     {
-        if(Input.anyKeyDown && !isLoading) // 任意のキーが押されたら
+        if(Input.GetKeyDown(KeyCode.Space) && !isLoading) // 任意のキーが押されたら
         {
             isLoading = true; // ロード中フラグを立てる
             LoadScene(); // シーンをロードする
         }
-    }
 
+        logo.transform.localPosition = new Vector3(0, Mathf.Sin(Time.time * 2) * 5, 0); // タイトルの位置を上下に動かす
+        
+    }
     public void LoadScene()
     {
         audioSource.PlayOneShot(start); // 音声を再生
